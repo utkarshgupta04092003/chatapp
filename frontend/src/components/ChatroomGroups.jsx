093f78451e6c8bacf97a171d926dfd5584d1473d
Utkarsh Gupta <utkarshgupta04092003@gmail.com>
2024-03-17 23:09:43 +0530

    delete all message before deleting the chatroom, completed

--- frontend/src/components/ChatroomGroups.jsx
+++ frontend/src/components/ChatroomGroups.jsx
@@ -8,7 +8,7 @@ export default function ChatroomGroups({ allChatrooms, chagneGroup, selectedGrou
     return (!allChatrooms) ? (<h2 className='text-center text-xl font-semibold mt-5 border border-gray-500'>Loading Chatrooms</h2>) : (
         <div className="flex-1 overflow-y-auto mt-5">
             <h1 className="text-2xl font-semibold mx-4"> All Chatrooms</h1>
-            {allChatrooms.length == 0 && <h2 className='text-center text-lg py-3 font-semibold text-black mt-5 border border-gray-500 rounded-lg mx-4'>No Chatrooms found</h2>}
+            {allChatrooms.length == 0 && <h2 className='text-center text-lg py-2 font-semibold text-black mt-5 border border-gray-500 rounded-lg mx-4'>No Chatrooms found</h2>}
             <ul className="px-4">
                 {
                     allChatrooms?.map((chatroom) => (
